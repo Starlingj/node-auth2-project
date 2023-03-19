6fefd288a242a62fbaf1a391721f6c8c79140694
james starling <e.s.gbeast@gmail.com>
2023-03-19 12:46:35 -0400

    added auth middleware and router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,42 +1,71 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../users/users-model");
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
     response:
     status 201
     {
-      "user"_id: 3,
       "username": "anna",
+      "user"_id: 3,
       "role_name": "angel"
     }
    */
+
+  const { username, password} = req.body;
+  const role_name = req.role_name;
+  const hash = bcrypt.hashSync(password, 8);
+  User.add({ username, password: hash, role_name })
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(next);
 });
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
     response:
     status 200
     {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
-
     The token must expire in one day, and must provide the following information
     in its payload:
-
     {
       "subject"  : 1       // the user_id of the authenticated user
       "username" : "bob"   // the username of the authenticated user
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  if (bcrypt.compareSync(req.body.password, req.user.password)) {
+   const token = buildToken(req.user);
+    res.status(200).json({
+      message: `${req.user.username} is back!`,
+      token
+    });
+  } else {
+    next({ status: 401, message: "Invalid credentials" });
+  }
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id, //user.id would not work because user is a user object, not a user object with an id
+    username: user.username,
+    role_name: user.role_name
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
